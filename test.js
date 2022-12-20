46e639665ae688e249eb3f9a5bf7a05a44bfd488
shanmugarajmuthuvel <shanmugarajmuthuvel1777@yahoo.com>
2022-12-20 12:12:39 +0530

    array method

---
+++ test.js
@@ -0,0 +1,25 @@
+ // var food=[]
+ // var a =prompt("Enter the food length")
+ // for(i=1; i<=a; i++){
+ // var b=prompt("Enter the food items")
+  // document.write(food[i]=b)
+  // document.write("<br>")
+  
+ // }
+  // console.log(food)
+   var food=[]
+     var a=prompt("Enter the list of Food")
+	 for(i=0; i<a; i++){
+		    food[i]=prompt("Enter Your Food")
+	 }
+	 document.write(food)
+	 document.write("<br>")
+	 var chf=prompt("Enter the change food")
+	 for(i=chf;i<=chf; i++){
+		 food[i]=prompt("Enter your Food")
+	 }
+	 document.write(food)
+			
+   
+   
+   
